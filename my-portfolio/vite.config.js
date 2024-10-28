c0ee6f3553b1ea510c809ea863fbdfce08ea0d63
Zack Agba <Zackagba7@gmail.com>
2024-10-28 01:23:26 +0100

    Hotfixes

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Set your GitHub username and repository name
+const GITHUB_USERNAME = 'king101-bit';
+const GITHUB_REPO = 'Official_portfolio';
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: `https://${GITHUB_USERNAME}.github.io/${GITHUB_REPO}/`,
+});
