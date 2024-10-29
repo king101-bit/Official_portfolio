d2cc376b4dd4e01f963c448d4ba35398af01bae1
Zack Agba <Zackagba7@gmail.com>
2024-10-29 14:15:44 +0100

    Changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/official_portfolio/", // YOUR REPO NAME HERE
+  base: "./", // YOUR REPO NAME HERE
 });
