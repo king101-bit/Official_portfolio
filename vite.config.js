14df9a8223c46431008b2b20a536eaf02b2f83c6
king101-bit <king101-bit@users.noreply.github.com>
2024-10-29 10:27:24 +0000

    deploy: 317986105bac9fb0f7bc21770dd0b9aa47a214cb

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/official_portfolio/", // YOUR REPO NAME HERE
-});
